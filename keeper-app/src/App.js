582e6b864808a7989158936bb3839e0fe0533684
Figen Daghan <figendaghan06@gmail.com>
2023-12-07 13:53:23 +0300

    Keeper app updated

--- keeper-app/src/App.js
+++ keeper-app/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import CreateArea from './Components/CreateArea';
 import Footer from './Components/Footer';
@@ -5,12 +6,34 @@ import Header from './Components/Header';
 import Note from './Components/Note';
 
 function App() {
+const [notes, setNotes] = useState([]);
+
+const addNote = (newNote) => {
+setNotes(prevNotes => ([...prevNotes, newNote]));
+}
+
+const deleteNote = (id) => {
+setNotes(prevItem => (prevItem.filter((noteItem, index) => {
+  return index !== id
+})));
+}
   return (
     <div >
      <Header/>
-      <CreateArea/>   
+      <CreateArea
+      onAdd={addNote}
+      />   
        <Footer/>
-       <Note key={1} title="Note title" content="Note content"/>
+       {/* <Note key={1} title="Note title" content="Note content"/> */}
+       {notes.map((noteItem, index) => (
+        <Note
+        key={index}
+        id={index}
+        title={noteItem.title}
+        content={noteItem.content}
+        onDelete={deleteNote}
+        />
+       ))}
     </div>
   );
 }
--- keeper-app/src/Components/CreateArea.js
+++ keeper-app/src/Components/CreateArea.js
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
-function CreateArea() {
+function CreateArea(props) {
+const [note, setNote] = useState({
+    title:"",
+    content:""
+});
+const [isExpended, setIsExpended] = useState(false);
+
+const handleChange = (event) => {
+const {name, value} = event.target;
+setNote(prevNote => ({...prevNote, [name]: value}));
+}
+
+    const submitNote = (event) => {
+    props.onAdd(note);
+    setNote({
+        title:"",
+        content:""
+    });
+    event.preventDefault();
+    }
+
+const Expend = () => {
+    setIsExpended(true);
+}
+
   return (
     <div>
       <form className='create-area'>
-        <input type="text" placeholder='title' />
-        <textarea placeholder='take a note' rows="3" ></textarea>
-        <button> <AddIcon/> </button>
-      </form>
+       {isExpended && ( <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder='Title' 
+          />)}
+        <textarea
+         onClick={Expend}
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder='Take a note...'
+           rows={isExpended ? "3" : "1"}
+            />
+
+            <Zoom in={isExpended}>
+            <Fab  onClick={submitNote}> 
+            <AddIcon/> 
+            </Fab>
+            </Zoom>
+        </form>
     </div>
   )
 }
