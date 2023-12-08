34a0694522f5573ced6c0b8fa41d03b705c2f6c2
Figen Daghan <figendaghan06@gmail.com>
2023-12-08 13:11:01 +0300

    Keeper app completed

--- keeper-app/src/Components/Note.js
+++ keeper-app/src/Components/Note.js
@@ -8,12 +8,14 @@ props.onDelete(props.id);
 }
 
   return (
+    <div className="cont">
     <div className='note'>
       <h1>{props.title}</h1>
       <p>{props.content}</p>
       <button onClick={handleClick}> 
         <DeleteIcon/>
          </button>
+    </div>
     </div>
   )
 }
